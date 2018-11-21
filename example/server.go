fc1d454756dd1f29e000080801ea9e243cc6c899
游剑涛 <youjiantao@gridsum.com>
2018-11-21 09:46:49 +0800

    update frontend

--- example/server.go
+++ example/server.go
@@ -5,6 +5,8 @@ import (
 	"github.com/gorilla/websocket"
 	"fmt"
 	"os"
+	"time"
+	"github.com/OhBonsai/croner"
 )
 
 var ch = make(chan string, 10)
@@ -13,13 +15,28 @@ var upgrader = websocket.Upgrader{
 	WriteBufferSize: 10240,
 }
 
-type CreateJobRequestJson struct {
+type JobS struct {
 	duration int
 	who string
 	what string
 }
 
+func (j JobS) Run() croner.JobRunReturn{
+	return croner.JobRunReturn{
+		Value: fmt.Sprintf("[%s] %s: %s",time.Now().Format(time.RFC850), j.who, j.what),
+	}
+}
+
+
+
 func CreateJob(c *gin.Context) {
+	var curJob JobS
+	err := c.BindJSON(&curJob)
+
+	if err != nil {
+		c.JSON(400, "Bad Request")
+	}
+
 
 }
 
@@ -40,9 +57,29 @@ func Echo(c *gin.Context) {
 	}
 }
 
+func Status(c *gin.Context) {
+	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		fmt.Sprintf("Failed to set websocket upgrader: %v", err)
+		os.Exit(1)
+	}
+
+	for {
+		time.Sleep(1 * time.Second)
+		conn.WriteJSON([]int{})
+	}
+}
+
+
 func main(){
+	croner.OnJobReturn(func(runReturn *croner.JobRunReturn) {
+		say := runReturn.Value.(string)
+		ch <- say
+	})
+
 	r := gin.Default()
 	r.POST("/job", CreateJob)
 	r.GET("/ws", Echo)
+	r.GET("/status", Status)
 	r.Run(":80")
 }
