6c3d8f60bae60f9e58398c864066f2d7cab1b247
游剑涛 <youjiantao@gridsum.com>
2018-11-27 20:20:03 +0800

    reformat code

--- example/server.go
+++ example/server.go
@@ -3,11 +3,13 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
+	"github.com/DeanThompson/ginpprof"
 	"fmt"
 	"os"
 	"time"
 	"github.com/OhBonsai/croner"
 	"net/http"
+	"sort"
 )
 
 var manager = croner.NewCronManager(croner.CronManagerConfig{
@@ -16,24 +18,22 @@ var manager = croner.NewCronManager(croner.CronManagerConfig{
 
 var ch = make(chan string, 10)
 var upgrader = websocket.Upgrader{
-	ReadBufferSize: 1,
+	ReadBufferSize:  1,
 	WriteBufferSize: 10240,
-	CheckOrigin: func(r *http.Request) bool {return true},
+	CheckOrigin:     func(r *http.Request) bool { return true },
 }
 
 type JobS struct {
-	Duration int `json:"duration"`
+	Duration int    `json:"duration"`
 	Who      string `json:"who"`
 	What     string `json:"what"`
 }
 
-
-func (j JobS) Run() croner.JobRunReturn{
+func (j JobS) Run() croner.JobRunReturn {
 	return croner.JobRunReturn{
-		Value: fmt.Sprintf("[%s] %s: %s",time.Now().Format(time.RFC850), j.Who, j.What),
+		Value: fmt.Sprintf("[%s] %s: %s", time.Now().Format(time.RFC850), j.Who, j.What),
 	}
 }
-
 
 func CreateJob(c *gin.Context) {
 	var curJob JobS
@@ -53,7 +53,6 @@ func CreateJob(c *gin.Context) {
 	c.JSON(200, "success")
 }
 
-
 func Echo(c *gin.Context) {
 	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
 	if err != nil {
@@ -65,19 +64,20 @@ func Echo(c *gin.Context) {
 	// push data to client
 	for {
 		select {
-			case msg := <-ch:
-				conn.WriteMessage(websocket.TextMessage, []byte(msg))
-			default:
-				continue
+		case msg := <-ch:
+			conn.WriteMessage(websocket.TextMessage, []byte(msg))
+		default:
+			continue
 		}
 	}
 }
 
 type StatusResp struct {
-	Name string `json:"name"`
-	Status string `json:"status"`
-	SuccessAndTotal string `json:"success"`
-	Next time.Time `json:"next"`
+	Name            string    `json:"name"`
+	Status          string    `json:"status"`
+	SuccessAndTotal string    `json:"success"`
+	Next            time.Time `json:"next"`
+	Eid             int       `json:"-"`
 }
 
 // all job status
@@ -92,22 +92,25 @@ func Status(c *gin.Context) {
 	for {
 		returnResponse := []StatusResp{}
 
-		for _, v := range manager.JobMap{
+		for _, v := range manager.JobMap {
 			innerJob := v.Inner.(JobS)
 			returnResponse = append(returnResponse, StatusResp{
-				Name: innerJob.Who,
-				Status: v.Status(),
+				Name:            innerJob.Who,
+				Status:          v.Status(),
 				SuccessAndTotal: fmt.Sprintf("%d/%d", v.SuccessCount, v.TotalCount),
-				Next: v.Next,
+				Next:            v.Next,
+				Eid:             int(v.Id),
 			})
 		}
+		sort.Slice(returnResponse, func(i, j int) bool {
+			return returnResponse[i].Eid < returnResponse[j].Eid
+		})
 		conn.WriteJSON(returnResponse)
 		time.Sleep(1 * time.Second)
 	}
 }
 
-
-func main(){
+func main() {
 	// push job run return into channel
 	croner.OnJobReturn(func(runReturn *croner.JobRunReturn) {
 		say := runReturn.Value.(string)
@@ -118,10 +121,11 @@ func main(){
 
 	r := gin.Default()
 	r.LoadHTMLGlob("example/*.html")
+	ginpprof.Wrapper(r)
 	r.POST("/job", CreateJob)
 	r.GET("/echo", Echo)
 	r.GET("/status", Status)
-	r.GET("/", func(c *gin.Context){
+	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", nil)
 	})
 	r.Run(":8000")
